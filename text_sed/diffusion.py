Fix prefix masking arg

text_sed/diffusion.py
@@ -264,7 +264,7 @@ class TextSed(nn.Module):
                 )
             elif self.mask_type == "prefix":
                 masks.append(
-                    get_prefix_mask(num_pos, prefix_rate=self.mask_prefix_rate)
+                    get_prefix_mask(num_pos, rate=self.mask_prefix_rate)
                 )
         return torch.stack(masks).to(device)
 

text_sed/layers.py
@@ -556,7 +556,8 @@ def get_prefix_mask(seq_len: int, rate: float = 0.75) -> torch.Tensor:
     indices = torch.arange(0, seq_len)
     prefix_len = random.randint(0, int(seq_len * rate) - 1)
     if random.random() > 0.5:
-        return torch.ones((seq_len), dtype=torch.bool)
+        # At random, ignore the prefix mask
+        return torch.zeros((seq_len), dtype=torch.bool)
     return indices > prefix_len
 
 